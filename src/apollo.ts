86ecdffdc00794ddfa11fdec356b3364ebec28cd
Kwangho <khseok1060@gmail.com>
2018-08-16 22:15:03 +0900

    #2.2 Apollo Setup part Two

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,13 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { Operation } from "apollo-boost";
 
 const client = new ApolloClient({
+  request: async(opreation: Operation) => {
+    opreation.setContext({
+      headers: {
+        "X-JWT": localStorage.getItem("jwt") || ""
+      }
+    })
+  },
   uri: "http://localhost:4000/graphql"
 });
 
