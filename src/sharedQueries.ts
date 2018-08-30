0e72224a844c418d694e24581b81d0e8f3838dab
Kwangho <khseok1060@gmail.com>
2018-08-30 21:10:58 +0900

    #2.32 Home Sidebar Query

--- src/sharedQueries.ts
+++ src/sharedQueries.ts
@@ -1,11 +1,5 @@
 import { gql } from "apollo-boost";
 
-export const LOG_USER_IN = gql`
-  mutation logUserIn($token: String!) {
-    logUserIn(token: $token) @client
-  }
-`;
-
 export const USER_PROFILE = gql`
   query userProfile {
     GetMyProfile {
