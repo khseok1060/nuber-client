1703c0f06496a550d1f09c50ffdfd7dc7c3fb824
Kwangho <khseok1060@gmail.com>
2018-09-02 12:22:20 +0900

    #2.36 EditProfile Screen part One

--- src/Routes/EditAccount/index.tsx
+++ src/Routes/EditAccount/index.tsx
@@ -1,5 +1,2 @@
-import React from "react";
-
-const R = () => <span>Lalala</span>
-
-export default R;
+import EditAccountContainer from "./EditAccountContainer";
+export default EditAccountContainer;
