11d07dc0438a04eae72661830b7dc581ab95d34d
Kwangho <khseok1060@gmail.com>
2018-09-19 19:15:33 +0900

    #2.55 HomeScreen Moving with the User

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -32,7 +32,7 @@ class HomeContainer extends React.Component<IProps, IState> {
     this.mapRef = React.createRef();
   }
   public componentDidMount() {
-    navigator.geolocation.watchPosition(
+    navigator.geolocation.getCurrentPosition(
       this.handleGeoSuccess,
       this.handleGeoError
     );
@@ -108,7 +108,11 @@ class HomeContainer extends React.Component<IProps, IState> {
     );
   };
   public handleGeoWatchSuccess = (position: Position) => {
-    return;
+    const {
+      coords: { latitude, longitude }
+    } = position;
+    this.userMarker.setPosition({ lat: latitude, lng: longitude });
+    this.map.panTo({ lat: latitude, lng: longitude });
   };
   public handleGeoWatchError = () => {
     console.log("Error watching you");
