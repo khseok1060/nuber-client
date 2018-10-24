9e372bac26aa2ce67b727ca88e0d4a830486aba3
Kwangho <khseok1060@gmail.com>
2018-10-24 19:52:54 +0900

    #2.73 Get Ride Query part One

--- src/Routes/Ride/RidePresenter.tsx
+++ src/Routes/Ride/RidePresenter.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { getRide } from "src/types/api";
 import styled from "../../typed-components";
 
 const Container = styled.div``;
 
-const RidePresenter: React.SFC = () => <Container>Ride</Container>;
+interface IProps {
+  data?: getRide;
+}
+
+const RidePresenter: React.SFC<IProps> = () => <Container>Ride</Container>;
 
 export default RidePresenter;
